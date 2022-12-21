6a2557467f38ad37447e307f2b5a56492621d6e9
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-12-21 17:50:53 +0000

    Added more constants.

--- src/time.js
+++ src/time.js
@@ -3,23 +3,38 @@
 
 //Various time constants in order of precedence.
 const one_day = 1;
-const days_in_week = 7 * day;
+
+const days_in_week = 7 * one_day;
 const weeks_in_year = 52;
 const days_in_year = weeks_in_year * days_in_week;
 const months_in_year = 12;
+
 const days_in_month_average = days_in_year / months_in_year;
-const days_in_leap_year = days_in_year + one_day;
 const days_in_leap_year_month_average = days_in_leap_year / days_in_month_average;
+const days_in_leap_year = days_in_year + one_day;
 
 const one_second = 1;
+const seconds_in_minute = 60;
+const minute_seconds = one_second * seconds_in_minute;
+const seconds_in_hour = 60;
+const hour_seconds = minute_seconds * seconds_in_hour;
+const hours_in_day = 24;
 
-//sec == second.
-const minute_sec = one_second * 60;
-const hour_sec = minute_sec * 60;
-const day_sec = hour_sec * 24;
-const week_sec = day_sec * 7;
-const year_sec = week_sec * 365;
-const leap_year_sec = year_sec + day_sec;
+const one_day_seconds = hour_seconds * hours_in_day;
+const one_week_seconds = one_day_seconds * days_in_week;
+const one_year_seconds = one_week_seconds * days_in_year;
+
+const leap_year_seconds = one_year_seconds + day_seconds;
+
+const minutes_in_hour = 60;
+const minutes_in_day = minutes_in_hour * hours_in_day;
+const minutes_in_week = minutes_in_day * days_in_week;
+const minutes_in_year = minutes_in_day * days_in_year;
+const minutes_in_leap_year = minutes_in_day * days_in_leap_year;
+
+const hours_in_week = hours_in_day * days_in_week;
+const hours_in_year = hours_in_day * days_in_year;
+const hours_in_leap_year = hours_in_day * days_in_leap_year;
 
 function is_leap_year(year) {
     if (year % 4 != 0) return false;
@@ -40,3 +55,4 @@ function days_in_month_of_year(month, year) {
 function seconds_in_month_of_year(month, year) {
     return days_in_month_of_year(month, year) * day_sec;
 }
+
