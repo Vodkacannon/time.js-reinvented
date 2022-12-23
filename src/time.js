9e074f7dfb913cc9e6b89ea3871479f46dcf0d37
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-12-23 01:04:20 +0000

    Added more functions.

--- src/time.js
+++ src/time.js
@@ -3,8 +3,8 @@
 
 //Various time constants in order of precedence.
 const one_day = 1;
-
 const days_in_week = 7 * one_day;
+
 const weeks_in_year = 52;
 const days_in_year = weeks_in_year * days_in_week;
 const months_in_year = 12;
@@ -13,17 +13,16 @@ const days_in_month_average = days_in_year / months_in_year;
 const days_in_leap_year_month_average = days_in_leap_year / days_in_month_average;
 const days_in_leap_year = days_in_year + one_day;
 
+const hours_in_day = 24;
+
 const one_second = 1;
 const miliseconds_in_second = one_second * 1000;
 const miliseconds_in_minute = miliseconds_in_second * seconds_in_minute;
 const seconds_in_minute = one_second * 60;
 const seconds_in_hour = seconds_in_minute * minutes_in_day;
-const hours_in_day = 24;
-
 const seconds_in_day = seconds_in_hour * hours_in_day;
 const seconds_in_week = seconds_in_day * days_in_week;
 const seconds_in_year = seconds_in_week * days_in_year;
-
 const seconds_in_leap_year = seconds_in_year + seconds_in_day;
 
 const minutes_in_hour = 60;
@@ -61,16 +60,20 @@ function days_in_month_of_year(month, year) {
     else return 31;
 }
 
-function seconds_in_month_of_year(month, year) {
+function seconds_in_month_of_a_year(month, year) {
     return days_in_month_of_year(month, year) * seconds_in_day;
 }
 
-function minutes_in_month_of_year(month, year) {
+function minutes_in_month_of_a_year(month, year) {
     return days_in_month_of_year(month, year) * minutes_in_day;
 }
 
-function hours_in_month_of_year(month, year) {
+function hours_in_month_of_a_year(month, year) {
     return days_in_month_of_year(month, year) * hours_in_day;
+}
+
+function weeks_in_month_of_a_year(month, year) {
+    return days_in_month_of_year(month, year) / days_in_week;
 }
 
 function get_seconds_in_year(year) {
@@ -88,12 +91,29 @@ function get_hours_in_year(year) {
     else { return hours_in_year; }
 }
 
-function seconds_between_hours(hour1, hour2) {
-    return (hour2 - hour1) * seconds_in_hour;
+function seconds_between_minutes(minute_one, minute_two) {
+    return (minute_one - minute_two) * seconds_in_minute;
 }
 
-function minutes_between_hours(hour1, hour2) {
-    return (hour2 - hour1) * minutes_in_hour;
+function seconds_between_hours(hour_one, hour_two) {
+    return (hour_two - hour_one) * seconds_in_hour;
+}
+
+function seconds_between_days(day_one, day_two) {
+    return (day_two - day_one) * seconds_in_day;
+}
+
+//This will take some work due to the fact that months are not all the same length.
+function seconds_between_months(month_one, month_two) {
+    return (month_two - month_one) * seconds_in_month;
+}
+
+function seconds_between_years(year_one, year_two) {
+    return (year_two - year_one) * seconds_in_year;
+}
+
+function minutes_between_hours(hour_one, hour_two) {
+    return (hour_two - hour_one) * minutes_in_hour;
 }
 
 async function sleep_for_miliseconds(miliseconds) {
@@ -125,10 +145,19 @@ function get_date() {
     return new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds());
 }
 
-function get_difference_in_seconds(date1, date2) {
+function difference_between_dates_as_seconds(date_one, date_two) {
     let seconds = 0;
-    seconds += seconds_between_hours(date1.hour, date2.hour);
-    seconds += (date2.minute - date1.minute) * seconds_in_minute;
+    seconds += seconds_between_years(date_one.year, date_two.year);
+    seconds += seconds_between_hours(date_one.hour, date_two.hour);
+    seconds += seconds_between_minutes(date_one.minute, date_two.minute);
     seconds += (date2.second - date1.second);
     return seconds;
+}
+
+function difference_between_dates_as_minutes(date_one, date_two) {
+    let minutes = 0;
+    minutes += minutes_between_years(date_one.year, date_two.year);
+    minutes += minutes_between_hours(date_one.hour, date_two.hour);
+    minutes += (date_two.minute - date_one.minute);
+    return minutes;
 }
